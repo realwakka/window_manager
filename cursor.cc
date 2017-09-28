5e779bd226e02686b5ea6badd34010b8c940c2c0
realwakka <realwakka@gmail.com>
2017-09-28 23:35:06 +0000

    cursor

--- cursor.cc
+++ cursor.cc
@@ -4,19 +4,23 @@
 
 namespace wm {
 
-Cursor::Cursor() : x_(0), y_(0) {}
+Cursor::Cursor() : x_(0), y_(0), delegate_(new NormalCursorDelegate) {}
 
 void Cursor::Paint(DrmInfo& drm)
 {
+  delegate_->Paint(*this, drm);
+}
+
+void NormalCursorDelegate::Paint(const Cursor& cursor, DrmInfo& drm)
+{
   for (int j=0;j<drm.cnt_;j++) {
     int col=0x0;
-    for (int y=y_;y<10;y++)
-      for (int x=x_;x<10;x++) {
+    for (int y=cursor.GetY();y<10;y++)
+      for (int x=cursor.GetX();x<10;x++) {
         int location=y*(drm.fb_w_[j]) + x;
         *(((uint32_t*)drm.fb_base_[j])+location)=col;
       }
   }
-
 }
   
 
